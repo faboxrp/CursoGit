d9a6c250fd48160adbd133ab7187b5f0a8ee3ee2
faboxrp <faboxrp@gmail.com>
2022-09-08 19:21:26 -0500

    Update kivy_pong.py

--- kivy_pong.py
+++ kivy_pong.py
@@ -9,10 +9,10 @@ class LoginScreen(GridLayout):
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
         self.cols = 2
-        self.add_widget(Label(text='Usuario'))
+        self.add_widget(Label(text='Usuario:'))
         self.username = TextInput(multiline=False)
         self.add_widget(self.username)
-        self.add_widget(Label(text='contraseña'))
+        self.add_widget(Label(text='Contraseña:'))
         self.password = TextInput(password=True, multiline=False)
         self.add_widget(self.password)
 
